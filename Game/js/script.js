52aa0b3a8a3d1db1e5d5a2eebf93e322baccadc5
Алексей Андрияненко <lex_flame@mail.ua>
2019-03-28 11:32:25 +0200

    'добавил убогий интерфейс'

--- Game/js/script.js
+++ Game/js/script.js
@@ -1,26 +1,75 @@
 var line = document.querySelector('.container-line');
 var timer = document.getElementById('input');
 
+var startBtn = document.getElementById('start-button');
+var cancelBtn = document.getElementById('cancel-button');
+
+var lengthOne = document.getElementById('length-one');
+var lengthTwo = document.getElementById('length-two');
+
+var weightOne = document.getElementById('weight-one');
+var weightTwo = document.getElementById('weight-two');
+
+
+// Free fall speed
+var g = 9.8;
+
+
 
 
 function  Rotation(elem){
   var elem = elem;
+  
 
-  function rotate(){
+  lengthOne.oninput = function(){
+    l1 = this.value;
+  }
   
-    var length1 = l1; // First length
-    var length2 = l2; // Second length
+  lengthTwo.oninput = function(){
+    l2 = this.value;
+  }
   
-    var Force1 = F1; // First load
-    var Force2 = F2; // Second load
+  weightOne.oninput = function(){
+    m1 = this.value;
+  }
   
-    var Moment1 = -(Force1 * length1); // First moment of force
-    var Moment2 = (Force2 * length2);  // Second moment of force
-  
-    var Moment0 = Moment1 + Moment2;
-  
-    line.style = 'transform: rotate('+ Moment0 +'deg);transition: all 2s;';
-  
+  weightTwo.oninput = function(){
+    m2 = this.value;
   }
 
+  function rotate(){
+
+    // Counting loads
+    F1 = Math.round(m1 * g);
+    F2 = Math.round(m2 * g);
+    l1 = l1/100;
+    l2 = l2/100;
+
+    // Counting moments of force
+    M1 = -(F1 * l1); // negative rotation
+    M2 =   F2 * l2;  // positive rotation
+
+    M0 = M1 + M2; // rotation angle
+    elem.style = 'transform: rotate('+ M0 +'deg);transition: all 2s;';
+  }
+
+  function cancel(){
+    elem.style = 'transform: rotate(0deg);transition: all 2s';
+  }
+
+  this.rotate = rotate;
+  this.cancel = cancel;
 }
+
+
+var leverArm = new Rotation(line);
+
+
+
+startBtn.addEventListener('click', function(){
+  leverArm.rotate();
+})
+
+cancelBtn.addEventListener('click', function(){
+  leverArm.cancel();
+})
